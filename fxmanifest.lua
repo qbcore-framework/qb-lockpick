fe34183117e49f19811ad16e1ea13606dd53ad37
Harley <83428883+Harley2787@users.noreply.github.com>
2022-08-27 02:01:08 +0100

    FIXED FOR SOME COUNTRIES
    FIXED FOR SOME COUNTRIES WHO COULDNT ACCESS DUE TO WAR

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,8 +1,8 @@
 fx_version 'cerulean'
 game 'gta5'
 
-description 'QB-Lockpick'
-version '1.0.0'
+description 'QB-Lockpick | Fixed by Harley#6796 for Countries who cant access certain websites!'
+version '1.0.2'
 
 ui_page 'html/index.html'
 
@@ -11,6 +11,9 @@ client_script 'client/main.lua'
 files {
     'html/index.html',
     'html/script.js',
+    'html/script2.js',
+    'html/jquery.js',
+    'html/collar.png',
     'html/style.css',
     'html/reset.css'
 }
